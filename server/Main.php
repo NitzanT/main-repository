aa77104756da2e2d4b070125264f1f5f748a46b0
NitzanT <nitzant@users.noreply.github.com>
2017-04-01 19:00:45 +0300

    index change

--- server/Main.php
+++ server/Main.php
@@ -28,20 +28,20 @@ class Main
             $uname = strip_tags($_POST['username']);
             $umail = strip_tags($_POST['username']);
             $upass = strip_tags($_POST['password']);
-            if(preg_match("/[\W]+/", $uname) || strlen($uname) == 0) {
-                $this->msg = $this->msg . "invalid username!";
+//            if(preg_match("/[\W]+/", $uname) || strlen($uname) == 0) {
+//                $this->msg = $this->msg . "invalid username!";
+//            }
+//            echo $this->msg;
+//            if ($this->msg=="") {
+            if ($login->doLogin($uname, $umail, $upass)) {
+                echo "login successfully!";
             }
-            echo $this->msg;
-            if ($this->msg=="") {
-                if ($login->doLogin($uname, $umail, $upass)) {
-                    echo "login successfully!";
-                }
-                else {
-                    echo "Wrong Details!";
-                }
+            else {
+                echo "Wrong Details!";
             }
-            $this->msg = "";
         }
+//            }
+//            $this->msg = "";
         if(isset($_POST['register'])) {
             $uname = trim($_POST['user_name']);
             $umail = trim($_POST['user_mail']);
